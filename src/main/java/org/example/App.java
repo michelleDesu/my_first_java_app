51f255ed64c12cf94acdce2befaac1234a218569
michelle <michelle.johansson85@gmail.com>
2020-04-07 13:42:08 +0200

    Added some exercises from Java fundamental slides *variables *assingment operators *Car object *user input

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,4 +1,5 @@
 package org.example;
+import java.util.Scanner;
 
 /**
  * Hello world!
@@ -8,6 +9,56 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! from Michelle" );
-    }
+        System.out.println( "Hello, \nMichelle!" );
+
+        String my_first_name = "Michelle";
+        System.out.println(my_first_name);
+
+        String first = "Michelle",
+                last = "Johansson";
+        System.out.println(first + " " + last);
+
+        float firstNum = 30.5f,
+                secondNum = 20.2f,
+                result = firstNum + secondNum;
+        System.out.println(result);
+
+        int score = 65;
+
+        if(score >= 65){
+            System.out.println("you passed");
+        }else{
+            System.out.println("You did not pass");
+        }
+
+        Scanner obj = new Scanner( System.in);
+       System.out.println("\n\nType your name here");
+
+       String name = obj.nextLine();
+        System.out.println(name);
+
+
+        class Car{
+                String brand,
+                        regNr,
+                        ownerName;
+                int     maxSpeed;
+                void print() {
+                    System.out.println(
+                            "Owner: " + ownerName + "\n" +
+                                    "Brand: " + brand + "\n" +
+                                    "Regnumber: " + regNr + "\n" +
+                                    "Max speed: " + maxSpeed + "\n"
+                    );
+                }
+        }
+
+        Car volvo = new Car();
+        volvo.brand = "Volvo";
+        volvo.regNr = "HSJ520";
+        volvo.maxSpeed = 256;
+        volvo.ownerName = "Ask av Grane";
+        volvo.print();
+
+        }
 }
